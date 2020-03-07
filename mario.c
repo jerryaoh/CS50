6c40b055d4e26aeb866e62d11d56c20a84c32f4e
jerryaoh <jerryaoh@users.noreply.github.com>
2020-03-07 11:02:07 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

---
+++ mario.c
@@ -0,0 +1,24 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int n, current_row;
+do{
+    n = get_int("Height: ");
+}
+while(n < 1);
+
+for(current_row = 1; current_row <= n; current_row++)
+    {
+    for(int i = 0; i < n - current_row; i++)
+        {
+        printf(" ");
+        }
+    for(int j = 0; j < current_row; j++)
+        {
+        printf("#");
+        }
+    printf("\n");
+    }
+}
