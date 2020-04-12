9b6d90cbe994209cedfc533356620c60cd3bca72
jerryaoh <jerryaoh@users.noreply.github.com>
2020-04-12 12:43:17 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -1,6 +1,14 @@
 #include "helpers.h"
 #include <math.h>
 
+typedef struct
+{
+    int blue;
+    int red;
+    int green;
+}
+matrix;
+
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -97,5 +105,106 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    return;
+    matrix GX[height][width], GY[height][width], Final[height][width];
+
+    // Computing GX
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            for (int n = -1; n < 2; n += 2)
+            {
+                for (int m = -1; m < 2; m++)
+                {
+                    if (i + m < 0 || i + m >= height || j + n < 0 || j + n >= width)
+                    {
+                        continue;
+                    }
+
+                    else
+                    {
+                        if (m == -1 || m == 1)
+                        {
+                            GX[i][j].blue += image[i + m][j + n].rgbtBlue * n;
+                            GX[i][j].red += image[i + m][j + n].rgbtBlue * n;
+                            GX[i][j].green += image[i + m][j + n].rgbtBlue * n;
+                        }
+
+                        else if (m == 0)
+                        {
+                            GX[i][j].blue += image[i + m][j + n].rgbtBlue * 2 * n;
+                            GX[i][j].red += image[i + m][j + n].rgbtRed * 2 * n;
+                            GX[i][j].green += image[i + m][j + n].rgbtGreen * 2 * n;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Computing GY
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            for (int m = -1; m < 2; m += 2)
+            {
+                for (int n = -1; n < 2; n++)
+                {
+                    if (i + m < 0 || i + m >= height || j + n < 0 || j + n >= width)
+                    {
+                        continue;
+                    }
+
+                    else
+                    {
+                        if (n == -1 || n == 1)
+                        {
+                            GY[i][j].blue += image[i + m][j + n].rgbtBlue * m;
+                            GY[i][j].red += image[i + m][j + n].rgbtBlue * m;
+                            GY[i][j].green += image[i + m][j + n].rgbtBlue * m;
+                        }
+
+                        else if (n == 0)
+                        {
+                            GY[i][j].blue += image[i + m][j + n].rgbtBlue * 2 * m;
+                            GY[i][j].red += image[i + m][j + n].rgbtRed * 2 * m;
+                            GY[i][j].green += image[i + m][j + n].rgbtGreen * 2 * m;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    // Calculate square root of GX^2 and GY^2
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            float s_blue = pow(GX[i][j].blue, 2) + pow(GY[i][j].blue, 2);
+            float s_green = pow(GX[i][j].green, 2) + pow(GY[i][j].green, 2);
+            float s_red = pow(GX[i][j].red, 2) + pow(GY[i][j].red, 2);
+            float x = round(sqrt(s_blue));
+            float y = round(sqrt(s_green));
+            float z = round(sqrt(s_red));
+
+            if (x > 255)
+            {
+                x = 255;
+            }
+            if (y > 255)
+            {
+                y = 255;
+            }
+            if (z > 255)
+            {
+                z = 255;
+            }
+
+            image[i][j].rgbtBlue = x;
+            image[i][j].rgbtGreen = y;
+            image[i][j].rgbtRed = z;
+        }
+    }
 }
